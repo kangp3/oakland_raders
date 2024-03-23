cdd4b2272bb86094ea12fba07d8b1daadd7b219c
Peter Kang <peterjkang@gmail.com>
2024-03-23 16:19:31 -0400

    Add tests

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,24 @@
-pub fn add(left: usize, right: usize) -> usize {
-    left + right
-}
+pub struct Pt(f64, f64, f64);
+
+pub struct Sphere;
+
+pub struct Ray;
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+    fn ray_hits_sphere() {
+        let sphere = Sphere::new(Pt(3.0, 4.0, 8.0), 1.0);
+        let ray = Ray::from_origin(Pt(0.5, 0.5, 1.0));
+        assert!(ray.hits(&sphere));
+    }
+
+    #[test]
+    fn ray_misses_sphere() {
+        let sphere = Sphere::new(Pt(3.0, 4.0, 8.0), 1.0);
+        let ray = Ray::from_origin(Pt(1.0, 0.5, 1.0));
+        assert!(!ray.hits(&sphere));
     }
 }
