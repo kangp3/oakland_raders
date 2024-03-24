e18b00bd3d8b2dd6da91e4596303fec78bd87bdf
Peter Kang <peterjkang@gmail.com>
2024-03-24 02:08:53 -0400

    Some linalg primitives

--- src/geometry.rs
+++ src/geometry.rs
@@ -1,7 +1,11 @@
+use image::Rgb;
+
+#[derive(Debug, PartialEq)]
 pub struct Pt(f64, f64, f64);
 
 pub trait Renderable {
     fn intersects(&self, r: &Ray) -> bool;
+    fn intersects_at(&self, r: &Ray) -> Vec<Pt>;
 }
 
 impl Pt {
@@ -9,14 +13,35 @@ impl Pt {
         Pt(x, y, z)
     }
 
-    pub fn origin() -> Self {
+    pub fn zero() -> Self {
         Pt(0.0, 0.0, 0.0)
+    }
+
+    pub fn scaled(&self, scale: f64) -> Self {
+        Pt(self.0 * scale, self.1 * scale, self.2 * scale)
+    }
+
+    pub fn mag(&self) -> f64 {
+        (self.0*self.0 + self.1*self.1 + self.2*self.2).sqrt()
+    }
+
+    pub fn unit(&self) -> Self {
+        let mag = self.mag();
+        Pt(self.0/mag, self.1/mag, self.2/mag)
+    }
+
+    pub fn dot(&self, other: &Pt) -> f64 {
+        self.0*other.0 + self.1*other.1 + self.2*other.2
     }
 }
 
 pub struct Sphere {
-    c: Pt,
-    r: f64,
+    c: Pt,           // Center
+    r: f64,          // Radius
+    color: Rgb<u8>,  // Color
+    lum: u8,         // Luminosity
+    spec: u8,        // Specular reflection
+    diff: u8,        // Diffuse reflection
 }
 
 pub struct Ray {
@@ -25,24 +50,34 @@ pub struct Ray {
 }
 
 impl Ray {
-    pub fn from_origin(p: Pt) -> Self {
-        Ray { o: Pt::origin(), p }
+    pub fn new(o: Pt, p: Pt) -> Self {
+        Ray { o, p }
     }
 
-    pub fn hits(&self, s: &Sphere) -> bool {
-        //                            (PxCx + PyCy + PzCz) ^2
-        // R^2 >= Cx^2 + Cy^2 + Cz^2 - -----------------------
-        //                              Px^2 + Py^2 + Pz^2
-        let denom = self.p.0 * self.p.0 + self.p.1 * self.p.1 + self.p.2 * self.p.2;
-        s.r * s.r * denom >=
-            (s.c.0 * s.c.0 + s.c.1 * s.c.1 + s.c.2 * s.c.2) * denom -
-            (s.c.0 * self.p.0 + s.c.1 * self.p.1 + s.c.2 * self.p.2).powi(2)
+    pub fn from_origin(p: Pt) -> Self {
+        Ray { o: Pt::zero(), p }
     }
 }
 
 impl Sphere {
     pub fn new(c: Pt, r: f64) -> Self {
-        Sphere { c, r }
+        Sphere { c, r, color: Rgb([255, 255, 255]), lum: 0, spec: 255, diff: 255 }
+    }
+
+    pub fn set_color(&mut self, color: Rgb<u8>) {
+        self.color = color;
+    }
+
+    pub fn set_lum(&mut self, lum: u8) {
+        self.lum = lum;
+    }
+
+    pub fn set_spec(&mut self, spec: u8) {
+        self.spec = spec;
+    }
+
+    pub fn set_diff(&mut self, diff: u8) {
+        self.diff = diff;
     }
 }
 
@@ -52,15 +87,53 @@ impl Renderable for Sphere {
         // R^2 >= Cx^2 + Cy^2 + Cz^2 - -----------------------
         //                              Px^2 + Py^2 + Pz^2
         let denom = r.p.0 * r.p.0 + r.p.1 * r.p.1 + r.p.2 * r.p.2;
-        self.r * self.r * denom >=
-            (self.c.0 * self.c.0 + self.c.1 * self.c.1 + self.c.2 * self.c.2) * denom -
-            (self.c.0 * r.p.0 + self.c.1 * r.p.1 + self.c.2 * r.p.2).powi(2)
+        self.r * self.r * denom
+            >= (self.c.0 * self.c.0 + self.c.1 * self.c.1 + self.c.2 * self.c.2) * denom
+                - (self.c.0 * r.p.0 + self.c.1 * r.p.1 + self.c.2 * r.p.2).powi(2)
+    }
+
+    fn intersects_at(&self, r: &Ray) -> Vec<Pt> {
+        let sum_of_squares = r.p.0*r.p.0 + r.p.1*r.p.1 + r.p.2*r.p.2;
+        let discriminant = (r.p.0 + r.p.1 + r.p.2).powi(2) -
+            sum_of_squares *
+            (self.c.0*self.c.0 + self.c.1*self.c.1 + self.c.2*self.c.2 -
+             self.r*self.r);
+        if discriminant < 0.0 {
+            return vec![];
+        }
+        if discriminant == 0.0 {
+            let scale = (r.p.0 + r.p.1 + r.p.2) / sum_of_squares;
+            return vec![r.p.scaled(scale)];
+        }
+        let sqrt_discriminant = discriminant.sqrt();
+        let scale1 = (r.p.0 + r.p.1 + r.p.2 - sqrt_discriminant) / sum_of_squares;
+        let scale2 = (r.p.0 + r.p.1 + r.p.2 + sqrt_discriminant) / sum_of_squares;
+        vec![r.p.scaled(scale1), r.p.scaled(scale2)]
     }
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+
+    #[test]
+    fn magnitude() {
+        let v = Pt(3.0, 4.0, -12.0);
+        assert_eq!(v.mag(), 13.0);
+    }
+
+    #[test]
+    fn unit_vector() {
+        let v = Pt(3.0, -4.0, 0.0);
+        assert_eq!(v.unit(), Pt(0.6, -0.8, 0.0));
+    }
+
+    #[test]
+    fn dot_product() {
+        let v1 = Pt(1.0, 3.0, -5.0);
+        let v2 = Pt(4.0, -2.0, -1.0);
+        assert_eq!(v1.dot(&v2), 3.0);
+    }
 
     #[test]
     fn ray_hits_sphere() {
--- src/main.rs
+++ src/main.rs
@@ -8,12 +8,14 @@ fn main() {
     let mut scene = Scene::new();
     scene.add_obj(Sphere::new(Pt::new(100.0, 200.0, 500.0), 200.0));
     scene.add_obj(Sphere::new(Pt::new(-50.0, -100.0, 500.0), 100.0));
-    let capture = scene.capture(500, 300);
+    let capture = scene.capture(500, 300); // TODO: Refactor dimensions
+    // TODO: Bouncing
 
     let mut img = RgbImage::new(500, 300);
 
     for x in 0..500 {
         for y in 0..300 {
+            // TODO: Make this a feature of a capture or a scene
             img.put_pixel(x, y, capture.get_pixel((x.try_into().unwrap(), y.try_into().unwrap())));
         }
     }
